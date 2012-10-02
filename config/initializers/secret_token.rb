247ef3fd3876528bfd20c777de5c0c0b33d7230d
Jim Caley <jcaley@mapquest.com>
2012-10-02 12:19:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0c2256b55a7cc2889f18986221d0287dd296c8f3942df2d92a302fdc0ece09dcbd9e508f9b6568910185899466292e2105b90aa66f677d2fc184eadc255fecdf'
